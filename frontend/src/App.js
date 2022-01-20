91768c9da2a373133f8a49242ba0ba72b0649890
Maddy246 <mayur.maluskar@gmail.com>
2022-01-20 12:42:26 +0530

    created schema for chats and users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomeScreen from "./Screen/HomeScreen";
+import ChatScreen from "./Screen/ChatScreen";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <HomeScreen />
+          </Route>
+          <Route path="/chats">
+            <ChatScreen />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
