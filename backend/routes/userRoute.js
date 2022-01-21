e526bf47d746ee78f3bc0261c62af1039ebdd7f6
Maddy246 <mayur.maluskar@gmail.com>
2022-01-22 03:31:34 +0530

    created routing

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userController");
+const { protect } = require("../middlewares/authUserMiddleware");
+
+const router = express.Router();
+
+router.route("/").post(registerUser).get(protect, allUsers);
+router.route("/login").post(authUser);
+
+module.exports = router;
