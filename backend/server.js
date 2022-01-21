d01ae9df4b48719fa42f5d8f4bc14f6cc56cacdb
Maddy246 <mayur.maluskar@gmail.com>
2022-01-22 04:07:18 +0530

    removed local data

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const chats = require("./data/data");
 const dotenv = require("dotenv");
 const connectDB = require("./config/dbConnection");
 const path = require("path");
