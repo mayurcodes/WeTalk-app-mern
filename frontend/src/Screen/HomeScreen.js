01a1392cdf12558c6bf4f4e00083a7c3a8c7b800
Maddy246 <mayur.maluskar@gmail.com>
2022-01-22 03:33:42 +0530

    added login and chat screens

--- frontend/src/Screen/HomeScreen.js
+++ frontend/src/Screen/HomeScreen.js
@@ -1,5 +1,68 @@
+import React, { useEffect } from "react";
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import Login from "../components/Auth/Login";
+import SignUp from "../components/Auth/SignUp";
+import { useHistory } from "react-router-dom";
+
 const HomeScreen = () => {
-  return <div>hello</div>;
+  const history = useHistory();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (user) history.push("/chats");
+  }, [history]);
+
+  return (
+    <Container centerContent maxW="xl">
+      <Box
+        d="flex"
+        justifyContent="center"
+        p="3"
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text color="black" fontFamily="Noto Serif" fontSize="3xl">
+          WeTalk
+        </Text>
+      </Box>
+      <Box
+        bg="white"
+        color="black"
+        w="100%"
+        p="4"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb="1rem">
+            <Tab w="50%">Login</Tab>
+            <Tab w="50%">Sign up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomeScreen;
